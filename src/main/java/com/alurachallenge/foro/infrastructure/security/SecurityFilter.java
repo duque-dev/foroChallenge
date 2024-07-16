7a22899d38ca26993a0ee73b451734cfb5e7d9ae
duque-dev <ali.duque.dev@gmail.com>
2024-07-16 16:02:52 -0500

    complete security auth

--- src/main/java/com/alurachallenge/foro/infrastructure/security/SecurityFilter.java
+++ src/main/java/com/alurachallenge/foro/infrastructure/security/SecurityFilter.java
@@ -1,5 +1,6 @@
 package com.alurachallenge.foro.infrastructure.security;
 
+import com.alurachallenge.foro.domain.repository.ClientRepository;
 import com.alurachallenge.foro.infrastructure.service.TokenService;
 import jakarta.persistence.Access;
 import jakarta.servlet.FilterChain;
@@ -7,6 +8,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -19,15 +22,22 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private ClientRepository clientRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = request.getHeader("Authorization");
-        if(token == null || token == ""){
-            throw new RemoteException("El token enviado no es válido");
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader != null){
+            var token = authHeader.replace("Bearer ", "");
+            var subject = tokenService.getSubject(token);
+            if(subject != null){
+                var client = clientRepository.findByUserName(subject);
+                var authentication = new UsernamePasswordAuthenticationToken(client, null,
+                        client.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
         }
-        token = token.replace("Bearer ", "");
-        System.out.println(token);
-        System.out.println(tokenService.getSubject(token));
         filterChain.doFilter(request,response);
     }
 }
